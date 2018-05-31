ec442554e3dfdcfbd24e220bb5d9a533d2cea259
Ben Coleman <colemanb@moravian.edu>
2018-05-31 15:52:55 -0400

    Add file and function-level documentation

--- src/project_reader.py
+++ src/project_reader.py
@@ -1,6 +1,18 @@
 
+"""
+Code to read the list of username and repo names.
+"""
+
 
 def read_project(filename):
+    """
+    Read the projects from a CSV file.
+
+    The fields of the file are username and repo name
+    :param filename: the path of the file to read (relative to package)
+    :return: list of dicts (username,repo_name)
+    :raises: FileNotFoundError if the file cannot be opened
+    """
 
     with open(filename) as f:
         pass
