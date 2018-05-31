fe0c3a9fef0e75e3990768381c251f8219859d6b
Ben Coleman <colemanb@moravian.edu>
2018-05-31 15:31:28 -0400

    Make the code a Python package

--- src/project_reader.py
+++ src/project_reader.py
@@ -1,2 +1,4 @@
 
 
+def read_project(filename):
+    pass
--- tests/test_project_reader.py
+++ tests/test_project_reader.py
@@ -1,4 +1,6 @@
 
+from project_reader import read_project
 
-def test_nothing():
-    pass
+
+def test_missing_file():
+    read_project('missing.csv')
