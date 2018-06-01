21e16dc0b714a705a36269f675c5162431f8379c
Daniel Stocker <stdrs02@moravian.edu>
2018-06-01 13:43:07 -0400

    Further tested and implemented repo_reader function.

--- src/repo_reader.py
+++ src/repo_reader.py
@@ -3,27 +3,76 @@ Code to connect to github and find information about repos.
 """
 
 import requests
+import json
 
-dict = {}
+
+dict = {'JohnSpp':'repo_stats'}
 path_root = 'http://api.github.com/'
+
+import os
+username = os.environ['GITHUB_USERNAME_DEV']
+password = os.environ['GITHUB_PASSWORD_DEV']
+
+
 def repo_reader():
+    global result
+    global commits
+    result = requests.get(get_commits(get_repo_path('JohnSpp')), auth=(username, password))
+    commits = json.loads(result.text)
 
 
+    name = get_commit_name()
+    message = get_commit_message()
+    date = get_commit_date()
+    insertions,deletions = get_insertions_deletions()
+    
+    #print("Name: " + name + '\n' + "Message: " + message + '\n' + "Date: " +date + '\n' + "Insertions: " + str(insertions) + '\n'
+     #     + "Deletions: " + str(deletions) + '\n')
 
-    return
+
 
 def set_dict(new_dict):
     global dict
     dict = new_dict
 
+
 def get_repo_path(name):
     repo_name = dict.get(name)
     return path_root + 'repos/' + name + '/' + repo_name
+
 
 def get_commits(repo_path):
     return repo_path + '/commits'
 
 
+def get_commit_name():
+    return commits[0]['commit']['author']['name']
 
 
+def get_commit_message():
+    return commits[0]['commit']['message']
+
+
+def get_commit_date():
+    return commits[0]['commit']['committer']['date']
+
+
+def get_sha():
+    return commits[0]['sha']
+
+
+def get_insertions_deletions():
+    result = requests.get(get_commits(get_repo_path('JohnSpp')) + '/' + get_sha())
+    stats = json.loads(result.text)
+    additions = stats['stats']['additions']
+    deletions = stats['stats']['deletions']
+    return additions, deletions
+
+
+
+
+
+
+
+repo_reader()
 
--- tests/test_repo_reader.py
+++ tests/test_repo_reader.py
@@ -1,13 +1,47 @@
 from repo_reader import *
 import pytest
 import requests_mock
+import json
+
 
 def test_setup():
     set_dict({'JohnSpp':'repo_stats'})
+    repo_reader()
+    # commits = json.loads(result.text)
+
 
 def test_path():
     assert(get_repo_path('JohnSpp') == 'http://api.github.com/repos/JohnSpp/repo_stats')
 
+
 def test_commit_path():
     assert (get_commits(get_repo_path('JohnSpp')) == 'http://api.github.com/repos/JohnSpp/repo_stats/commits')
+
+
+def test_connect():
+    result = requests.get(get_commits(get_repo_path('JohnSpp')))
+    assert (result.status_code == 200)
+
+
+def test_commit():
+    assert (get_commit_name() == "Alex Haug")
+
+
+def test_message():
+    assert (get_commit_message() == "Merge pull request #1 from bjcoleman/master\n\nCreate Read Project and Tests (initial work)")
+
+
+def test_get_date():
+    assert (get_commit_date() == '2018-05-31T19:58:40Z')
+
+
+def test_sha():
+    assert (get_sha() == '6f6b5f9334e61e5cc49c81897f40c61ec6ca86c2')
+
+
+def test_get_insertions():
+    x, y = get_insertions_deletions()
+    assert ( x == 47)
+    assert (y == 0)
+
 
